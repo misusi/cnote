256662b044d3d1df1d5276f2a4a496c4dd2cbbd0
misusi <55362709+misusi@users.noreply.github.com>
2022-10-04 22:25:01 -0400

    eatza pizza

--- cnote.c
+++ cnote.c
@@ -1,11 +1,6 @@
 /*
-TODO:
-    [-] Allow for appending file option (adding onto same set of notes)
-    [-] Use ^ to create some kind of heading
-    [-] Fix: on long lines, will sometimes print to the previious line
-    [+] Scroll page (move lines up by x amount) when "page" reaches the bottom
+TODO: Fix: on long lines, will sometimes print the previous line in curses
 */
-
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -18,94 +13,40 @@ TODO:
 #include <ncurses.h>
 #define MAX_LINE_INPUT_LENGTH 300
 
-static FILE *fp;
-static WINDOW* subwindow;
-void trim_whitespace(char *str);
-void substring(char *str, int start_index, int end_index);
-void clearScreen();
-void show_prompt(int tablevel);
-void quit(FILE *p_file_pointer, WINDOW *win);
-void sigIntHandler(int dummy);
-void reset_term();
-
-typedef struct LINE {
+typedef struct LINE
+{
     char * string;
     struct LINE *prev;
     struct LINE *next;
-    int is_header;
     int length;
 } LINE;
-
-typedef struct LINE_LIST {
+typedef struct LINE_LIST
+{
     LINE *first;
     LINE *last;
     int length;
     int capacity;
 } LINE_LIST;
 
-void line_list_init(LINE_LIST *line_list)
-{
-    line_list->capacity = 0;
-    line_list->length = 0;
-    line_list->first = NULL;
-    line_list->last = NULL;
-}
-
-void line_list_add_line(LINE_LIST *line_list, LINE *line)
-{
-    line->is_header = 0;
-    if (line_list->length == 0) {
-        line->prev = NULL;
-        line->next = NULL;
-        line_list->first = line;
-        line_list->last = line;
-    } else {
-        line->prev = line_list->last;
-        line->next = NULL;
-        line->prev->next = line;
-        line_list->last = line;
-    }
-    line_list->length++;
-}
-
-void line_list_remove_from_end(LINE_LIST *line_list)
-{
-    if (line_list->length == 0) {
-        return;
-    } else if (line_list->length == 1) {
-        free(line_list->last);
-        line_list->length--;
-    } else {
-        LINE *future_last = line_list->last->prev;
-        free(line_list->last);
-        line_list->last = future_last;
-        line_list->length--;
-    }
-}
-
-void line_list_remove_from_start(LINE_LIST *line_list)
-{
-    if (line_list->length == 0) {
-        return;
-    } else if (line_list->length == 1) {
-        free(line_list->first);
-        line_list->length--;
-    } else {
-        printf("1\n");
-        LINE *future_first = line_list->first->next;
-        printf("2\n");
-        free(line_list->first);
-        printf("3\n");
-        line_list->first = future_first;
-        printf("4\n");
-        line_list->length--;
-    }
-}
+static FILE *fp;
+static WINDOW* subwindow;
+static LINE_LIST line_list;
+void trim_whitespace(char *str);
+void substring(char *str, int start_index, int end_index);
+void clearScreen();
+void show_prompt(int tablevel);
+void quit(FILE *p_file_pointer, WINDOW *win, LINE_LIST* line_list);
+void sigIntHandler(int dummy);
+void reset_term();
+void line_list_init(LINE_LIST *line_list);
+void line_list_add_line(LINE_LIST *line_list, LINE *line);
+void free_list(LINE_LIST* list);
 
 int main(int argc, char **argv)
 {
     // Check args
-    if (argc < 2) {
+    if (argc < 2)
+    {
         printf("Usage: %s filename\n", argv[0]);
         return 1;
     }
@@ -114,41 +55,51 @@ int main(int argc, char **argv)
 
     char char_set_indent = '>';
     char char_set_outdent = '<';
-    char char_set_heading = '^';
     char char_bullet = '-';
-    char char_heading = '=';
 
     // ncurses
     initscr();
     cbreak();
     subwindow = newwin(LINES - 1, COLS, 1, 0);
-    //noecho();
-    //curs_set(1);
 
     // list of line inputs
-    LINE_LIST line_list;
     line_list_init(&line_list);
 
-
     // Check if file already exists
-    if (!access(argv[1], F_OK)) {
-        int overwrite_ok = 0;
-        mvprintw(0, 0, "%s %s", argv[1],
-                 "already exists. Should it be overwritten? (y/N): ");
-        char r[10];
+    if (!access(argv[1], F_OK))
+    {
+        mvprintw(0, 0, "%s \"%s\" %s ", "File", argv[1],
+                 "already exists. Overwrite, append, or neither (exit)? (o/a/N): ");
+        char r[20];
         getstr(r);
         trim_whitespace(r);
-        if (r > 0) {
-            if (tolower(r[0]) == 'y') {
-                overwrite_ok = 1;
+        if (r > 0)
+        {
+            if (tolower(r[0]) == 'n')
+            {
+                reset_term();
+                exit(1);
+            }
+            else if (tolower((r[0] == 'o')))
+            {
+                fp = fopen(argv[1], "w+");
+            }
+            else if (tolower((r[0] == 'a')))
+            {
+                fp = fopen(argv[1], "a+");
+                fprintf(fp, "%s", "\n");
+            }
+            else
+            {
+                reset_term();
+                exit(1);
             }
         }
-        if (!overwrite_ok) {
-            reset_term();
-            return 1;
+        else
+        {
+            fp = fopen(argv[1], "w+");
         }
     }
-    fp = fopen(argv[1], "w+");
 
     // fprint datetime
     time_t t = time(NULL);
@@ -156,12 +107,18 @@ int main(int argc, char **argv)
     char s[64];
     size_t ret = strftime(s, sizeof(s), "%c", tm);
     assert(ret);
-    fprintf(fp, "%s\n\n", s);
+    char divider[strlen(s)+1];
+    for(int i = 0; i < strlen(s); i++){
+        divider[i] = '-';
+    }
+    divider[strlen(s)] = '\0';
+    fprintf(fp, "\n%s\n%s\n%s\n\n", divider,s,divider);
 
     // input loop
     short curr_indent_level = 0;
     short saved_indent_level = curr_indent_level;
-    while (1) {
+    while (1)
+    {
 
         // ncurses print skeleton
         clear();
@@ -177,38 +134,41 @@ int main(int argc, char **argv)
         trim_whitespace(input);
 
         // check line for keywords (exit, quit, ...)
-        if (!strcmp(input, "quit")) {
-            quit(fp, subwindow);
+        if (!strcmp(input, "quit"))
+        {
+            quit(fp, subwindow, &line_list);
         }
-        if (!strcmp(input, "exit")) {
-            quit(fp, subwindow);
+        if (!strcmp(input, "exit"))
+        {
+            quit(fp, subwindow, &line_list);
         }
 
         // check input instruction (indent, outdent, reset, header, normal)
         int curr_index = 0;
         int input_start_index = 0;
-        int line_is_header = 0;
-        if (input[curr_index] == char_set_indent) {
+        if (input[curr_index] == char_set_indent)
+        {
             curr_indent_level++;
             input_start_index = 1;
-        } else if (input[curr_index] == char_set_outdent) {
+        }
+        else if (input[curr_index] == char_set_outdent)
+        {
             input_start_index = 1;
-            if (--curr_indent_level  < 0) {
+            if (--curr_indent_level  < 0)
+            {
                 curr_indent_level = 0;
             }
-            if (input[curr_index + 1] && input[curr_index + 1] == '>') {
+            if (input[curr_index + 1] && input[curr_index + 1] == '>')
+            {
                 curr_indent_level = 0;
                 input_start_index = 2;
             }
-        } else if (input[curr_index] == char_set_heading) {
-            input_start_index = 1;
-            new_line->is_header = 1;
-            //line_is_header = 1;
         }
         // Add tabs, header stuff
         substring(input, input_start_index, strlen(input));
         char output[MAX_LINE_INPUT_LENGTH] = "";
-        for (int i = 0; i < curr_indent_level; i++) {
+        for (int i = 0; i < curr_indent_level; i++)
+        {
             output[i] = '\t';
         }
         strcat(output, input);
@@ -231,16 +191,21 @@ int main(int argc, char **argv)
         // printing display
         int offset_y = 0, offset_x = 1;
         int n = 0;
-        if (line_list.length > LINES - 3) {
+        if (line_list.length > LINES - 3)
+        {
             current_line = line_list.last;
-            while (n < LINES - 2) {
-                mvwprintw(subwindow, LINES-3-n, offset_x, current_line->string);
+            while (n < LINES - 2)
+            {
+                mvwprintw(subwindow, LINES - 3 - n, offset_x, current_line->string);
                 current_line = current_line->prev;
                 n++;
             }
-        } else {
+        }
+        else
+        {
             int i = 1;
-            while (current_line != NULL) {
+            while (current_line != NULL)
+            {
                 mvwprintw(subwindow, i++, 1, current_line->string);
                 current_line = current_line->next;
             }
@@ -248,7 +213,7 @@ int main(int argc, char **argv)
         refresh();
         wrefresh(subwindow);
     }
-    quit(fp, subwindow);
+    quit(fp, subwindow, &line_list);
     exit(0);
 } // END main
 
@@ -258,7 +223,8 @@ void substring(char *str, int start_index, int end_index)
     if (end_index > strlen(str) - 1) end_index = strlen(str) - 1;
 
     int n = 0;
-    for (int i = start_index; i < end_index + 1; i++) {
+    for (int i = start_index; i < end_index + 1; i++)
+    {
         str[n] = str[n + start_index];
         n++;
     }
@@ -270,12 +236,15 @@ void trim_whitespace(char *str)
     if (str_len == 0) return;
     int first_non_space_index = 0;
     int last_non_space_index = str_len - 1;
-    while (first_non_space_index < str_len) {
+    while (first_non_space_index < str_len)
+    {
         if (!isspace(str[first_non_space_index])) break;
         first_non_space_index++;
     }
-    if (first_non_space_index < str_len - 1) {
-        while (last_non_space_index > 0) {
+    if (first_non_space_index < str_len - 1)
+    {
+        while (last_non_space_index > 0)
+        {
             if (!isspace(str[last_non_space_index])) break;
             last_non_space_index--;
         }
@@ -291,14 +260,16 @@ void show_prompt(int tablevel)
 {
     clearScreen();
     char prompt[100] = "";
-    for (int i = 0; i < tablevel; i++) {
+    for (int i = 0; i < tablevel; i++)
+    {
         strcat(prompt, "\t");
     }
     strcat(prompt, "cnote: ");
     printf("\r%s", prompt);
 }
-void quit(FILE *p_file_pointer, WINDOW *win)
+void quit(FILE * p_file_pointer, WINDOW * win, LINE_LIST * line_list)
 {
+    free_list(line_list);
     delwin(win);
     endwin();
     fclose(p_file_pointer);
@@ -309,7 +280,7 @@ void quit(FILE *p_file_pointer, WINDOW *win)
 }
 void sigIntHandler(int dummy)
 {
-    quit(fp, subwindow);
+    quit(fp, subwindow, &line_list);
 }
 void reset_term()
 {
@@ -318,4 +289,55 @@ void reset_term()
     erase();
     refresh();
     fflush(stdout);
+}
+
+void line_list_init(LINE_LIST * line_list)
+{
+    line_list->capacity = 0;
+    line_list->length = 0;
+    line_list->first = NULL;
+    line_list->last = NULL;
+}
+
+void line_list_add_line(LINE_LIST * line_list, LINE * line)
+{
+    if (line_list->length == 0)
+    {
+        line->prev = NULL;
+        line->next = NULL;
+        line_list->first = line;
+        line_list->last = line;
+    }
+    else
+    {
+        line->prev = line_list->last;
+        line->next = NULL;
+        line->prev->next = line;
+        line_list->last = line;
+    }
+    line_list->length++;
+}
+
+void free_list(LINE_LIST * list)
+{
+    if (list->length == 0) return;
+    else
+    {
+        if (list->length == 1)
+        {
+            free(list->first->string);
+        }
+        else
+        {
+            LINE *current_line = list->first;
+            while (current_line != list->last)
+            {
+                free(current_line->string);
+                current_line = current_line->next;
+                free(current_line->prev);
+            }
+            free(list->last->string);
+            free(list->last);
+        }
+    }
 }
